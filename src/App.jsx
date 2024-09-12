dbd4acd3df1d89da14852f220931d05ee5ad35f9
Felipe <felipemsoares22@gmail.com>
2024-09-12 10:44:17 -0300

    adicionando novas partes das atividades

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import FirstComponent from './components/FirstComponent'
-
+import Events from './components/Events'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
      <FirstComponent />
+     <Events/>
     </>
   )
 }
